60f34993e8806e4174aafa068f38c404084f9be8
Shahab Idrees <shahabchattha@gmail.com>
2022-04-23 18:25:23 +0500

    Created button instances

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1;
+        Button button2;
+        Button button3;
+        Button button4;
+        Button button5;
+        Button button6;
+        Button button7;
+        Button button8;
+        Button button9;
+        Button button0;
+        Button button_dot;
+        Button button_c;
+        Button button_plus;
+        Button button_minus;
+        Button button_divide;
+        Button button_multiply;
+        TextView textView;
+    }
+    protected void getValueOfButton(TextView temp, Button button) {
+
     }
 }
