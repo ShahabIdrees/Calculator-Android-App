6091a5add0fb7f6ff726d3c8dd1ebb65442f35b6
Shahab Idrees <shahabchattha@gmail.com>
2022-04-28 20:37:26 +0500

    Some Changes in Layout

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,36 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     protected double operand1 = 0;
     protected double operand2 = 0;
     protected double result = 0;
-    Boolean dot = false;
+    protected char operator;
+    Boolean is_OPerator_Clicked = false;
+    Boolean is_Dot_Clicked = false;
+    Boolean is_Equal_Clicked = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* Button button1;
-        Button button2;
-        Button button3;
-        Button button4;
-        Button button5;
-        Button button6;
-        Button button7;
-        Button button8;
-        Button button9;
-        Button button0;
+        Button button_c;
         Button button_dot;
-        Button button_c;
-        Button button_plus;
-        Button button_minus;
-        Button button_divide;
-        Button button_multiply;*/
-        Button button_c;
+        Button button_equal;
         TextView textView;
-        Button[] buttonArray = new Button[11];
+        Button[] buttonArray = new Button[10];
         buttonArray[0] = findViewById(R.id.button17);
         buttonArray[1] = findViewById(R.id.button18);
         buttonArray[2] = findViewById(R.id.button19);
@@ -46,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
         buttonArray[7] = findViewById(R.id.button10);
         buttonArray[8] = findViewById(R.id.button11);
         buttonArray[9] = findViewById(R.id.button22);
-        buttonArray[10] = findViewById(R.id.button23);
         button_c = findViewById(R.id.button21);
-
+        button_dot = findViewById(R.id.button23);
+        button_equal = findViewById(R.id.button25);
         Button[] operatorArray = new Button[4];
         operatorArray[0] = findViewById(R.id.button20);
         operatorArray[1] = findViewById(R.id.button24);
@@ -67,10 +58,38 @@ public class MainActivity extends AppCompatActivity {
         button_c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                is_Equal_Clicked = false;
+                is_OPerator_Clicked = false;
+                is_Dot_Clicked = false;
                 textView.setText(null);
+                textView.setHint("0");
             }
         });
+        button_dot.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                if(!is_Dot_Clicked){
+                    is_Dot_Clicked = true;
+                    getValueofButton(button_dot,textView);
+                }
+
+            }
+        });
+        button_equal.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(!is_Equal_Clicked){
+                    is_Equal_Clicked = true;
+
+
+                    }
+                    //result = calculateResult(operand1,operand2,operator);
+
+                }
+
+        });
     }
     public void custumOnClick(Button button, TextView textView) {
         button.setOnClickListener(new View.OnClickListener(){
@@ -81,29 +100,51 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void operatorOnClick(Button button, TextView textView){
-
-        CharSequence op1 = textView.getText();
-        char[] oper1 = new char[op1.length()];
-        for(int i = 0; i < op1.length(); i++){
-            if(op1.charAt(i) == '.')
-            oper1[i] = op1.charAt(i);
-        }
-        for(int i = 0; i < oper1.length; i++){
-            if(oper1[i] == '.'){
-                continue;
+        if(!is_OPerator_Clicked){
+            CharSequence op1 = textView.getText();
+            String oper1 = textView.getText().toString();
+            //operand1 = Double.valueOf(oper1);
             }
-            operand1 = ((int)oper1[i])  * 10 ^ (oper1.length - i);
+
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    if(!is_OPerator_Clicked){
+                        is_OPerator_Clicked = true;
+                        is_Dot_Clicked = false;
+                        is_Equal_Clicked = false;
+
+                        getOperator(button, textView);
+                    }
+
+
+                }
+            });
         }
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                
-                getValueofButton(button, textView);
-            }
-        });
+
+    public void getOperator(Button button, TextView textView){
+        operator = button.getText().charAt(0);
+        textView.setText(null);
+        
+
     }
     public void getValueofButton(Button button, TextView textView){
-        textView.append(" " + " " + button.getText());
+        textView.append(button.getText());
+    }
+    public double calculateResult(double op1, double op2, char oper){
+        if(oper == '/'){
+            return op1/op2;
+        }
+        else if(oper == 'X'){
+            return op1*op2;
+        }
+        else if(oper == '+'){
+            return op1+op2;
+        }
+        else if(oper == '-'){
+            return op1-op2;
+        }
+        return -1;
     }
 }
